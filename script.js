e3a5d7ee54f4b98b5e0432733d130e9fb0f45a1d
Dkboss743 <2019246@iiitdmj.ac.in>
2022-03-09 18:14:49 +0530

    created class

--- script.js
+++ script.js
@@ -10,13 +10,30 @@ const inputDistance = document.querySelector(".form__input--distance");
 const inputDuration = document.querySelector(".form__input--duration");
 const inputCadence = document.querySelector(".form__input--cadence");
 const inputElevation = document.querySelector(".form__input--elevation");
-let map, mapEvent;
-navigator.geolocation.getCurrentPosition(
-  function (position) {
+
+class App {
+  #map;
+  #mapEvent;
+  constructor() {
+    this._getPosition();
+    form.addEventListener("submit", this._newWorkout.bind(this));
+    inputType.addEventListener("change", this._toggleElevationField);
+  }
+  _getPosition() {
+    if (navigator.geolocation)
+      navigator.geolocation.getCurrentPosition(
+        this._loadMap.bind(this),
+        function () {
+          alert("Could not get your position");
+        }
+      );
+  }
+  _loadMap(position) {
     const latitude = position.coords.latitude;
     const longitude = position.coords.longitude;
     const coords = [latitude, longitude];
-    map = L.map("map").setView(coords, 13);
+
+    this.#map = L.map("map").setView(coords, 13);
 
     L.tileLayer(
       "https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}",
@@ -24,42 +41,41 @@ navigator.geolocation.getCurrentPosition(
         attribution:
           "Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community",
       }
-    ).addTo(map);
+    ).addTo(this.#map);
 
-    map.on("click", function (mapE) {
-      mapEvent = mapE;
-      form.classList.remove("hidden");
-      inputDistance.focus();
-    });
-  },
-  function () {
-    alert(`cannot locate your position`);
+    this.#map.on("click", this._showForm.bind(this));
   }
-);
-form.addEventListener("submit", function (e) {
-  e.preventDefault();
-  inputDistance.value =
-    inputDuration.value =
-    inputCadence.value =
-    inputElevation.value =
-      " ";
-  const { lat, lng } = mapEvent.latlng;
+  _showForm(mapE) {
+    this.#mapEvent = mapE;
+    form.classList.remove("hidden");
+    inputDistance.focus();
+  }
+  _toggleElevationField() {
+    inputElevation.closest(".form__row").classList.toggle("form__row--hidden");
+    inputCadence.closest(".form__row").classList.toggle("form__row--hidden");
+  }
+  _newWorkout(e) {
+    e.preventDefault();
+    inputDistance.value =
+      inputDuration.value =
+      inputCadence.value =
+      inputElevation.value =
+        " ";
+    const { lat, lng } = this.#mapEvent.latlng;
 
-  L.marker([lat, lng])
-    .addTo(map)
-    .bindPopup(
-      L.popup({
-        maxWidth: 250,
-        midWidth: 100,
-        autoClose: false,
-        closeOnClick: false,
-        className: "running-popup",
-      })
-    )
-    .setPopupContent("Exercise")
-    .openPopup();
-});
-inputType.addEventListener("change", function () {
-  inputElevation.closest(".form__row").classList.toggle("form__row--hidden");
-  inputCadence.closest(".form__row").classList.toggle("form__row--hidden");
-});
+    L.marker([lat, lng])
+      .addTo(this.#map)
+      .bindPopup(
+        L.popup({
+          maxWidth: 250,
+          midWidth: 100,
+          autoClose: false,
+          closeOnClick: false,
+          className: "running-popup",
+        })
+      )
+      .setPopupContent("Exercise")
+      .openPopup();
+  }
+}
+const app = new App();
