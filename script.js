d8f6eb8ce99ddda8c7c41d8b8d5541b504ab7734
Dkboss743 <2019246@iiitdmj.ac.in>
2022-03-09 18:54:22 +0530

    refactor

--- script.js
+++ script.js
@@ -10,6 +10,37 @@ const inputDistance = document.querySelector(".form__input--distance");
 const inputDuration = document.querySelector(".form__input--duration");
 const inputCadence = document.querySelector(".form__input--cadence");
 const inputElevation = document.querySelector(".form__input--elevation");
+class Workout {
+  date = new Date();
+  id = (Date.now() + " ").slice(-10);
+  constructor(coords, distance, duration) {
+    console.log(this.date);
+
+    this.coords = coords;
+    this.distance = distance;
+    this.duration = duration;
+  }
+}
+class Running extends Workout {
+  constructor(coords, distance, duration, cadence) {
+    super(coords, distance, duration);
+    this.cadence = cadence;
+    this.calcPace();
+  }
+  calcPace() {
+    this.pace = this.duration / this.distance;
+  }
+}
+class Cycling extends Workout {
+  constructor(coords, distance, duration, elevationGain) {
+    super(coords, distance, duration);
+    this.elevationGain = elevationGain;
+  }
+  calcSpeed() {
+    this.speed = this.distance / (this.duration / 60);
+    return this.speed;
+  }
+}
 
 class App {
   #map;
